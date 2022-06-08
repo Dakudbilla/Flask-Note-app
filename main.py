1c26a0653b5cf1d39249d3c5637359b9f6462c09
Akudbilla Daniel <akuddev@gmail.com>
2022-06-08 19:13:54 +0000

    added create and delete not feature to app

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from venv import create
-from website import create__app
+from website import create_app
 
-app = create__app()
+app = create_app()
 
 if __name__ == '__main__' :
     app.run(debug=True)
